58b511ebf2be34140f4c7f8ff1987b5ee7bbab3f
Lê Tùng Lâm <lam171ttt@gmail.com>
2022-07-20 21:31:53 +0700

    first commit

---
+++ Assignment-lamltpd04290/js/slick.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".list-subjects").slick({
+        slidesToShow: 6,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    });
+  });	
+  
